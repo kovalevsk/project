853fc258aef28d4b025be5cc82e334ae332747b3
Natali <example@gmail.com>
2020-11-05 19:53:43 +0200

    js first move

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 "use strict";
-const category = 'toys'
-console.log(4 + +"5")
-let incr = 10,
-    decr = 10
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?','')
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+}
+const a = prompt('Один из поледних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из поледних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','')
 
-console.log(++incr)
-console.log(--decr)
-console.log(5%2)
-console.log(2+2*2!=='6')
-
-const isChecked = false,
-      isClose = false
-      console.log(isChecked || !isClose)
+      personalMovieDB.movies[a] = b
+      personalMovieDB.movies[c] = d
+      console.log(personalMovieDB)
